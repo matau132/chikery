d65c54fd14a26bf5e57af10463d7bf4313ebd032
nam <matau135@gmail.com>
2020-11-17 20:36:38 +0700

    ingredient

---
+++ database/migrations/2020_11_17_130219_product_detail.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ProductDetail extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_details', function (Blueprint $table) {
+            $table->bigInteger('product_id');
+            $table->bigInteger('ingredient_id');
+            $table->integer('quantity');
+            $table->timestamps();
+            $table->foreign('product_id')->references('id')->on('product');
+            $table->foreign('ingredient_id')->references('id')->on('ingredient');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_details');
+    }
+}
