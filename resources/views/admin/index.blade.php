35f5b63426f56f8cb5295ff1a6f002f0b75e6112
bao <baousd@gmail.com>
2020-11-17 20:11:34 +0700

    test

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -11,7 +11,7 @@
               <div class="inner">
                 <h3>150</h3>
 
-                <p>New Orders</p>
+                <p>New Price</p>
               </div>
               <div class="icon">
                 <i class="ion ion-bag"></i>
