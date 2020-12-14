56e12250ae0370673979c860c6bc185c2279b69c
nam <matau135@gmail.com>
2020-12-14 21:20:05 +0700

    sua migrate

--- database/migrations/2020_12_14_125519_order_detail.php
+++ database/migrations/2020_12_14_125519_order_detail.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class OrderDetails extends Migration
+class OrderDetail extends Migration
 {
     /**
      * Run the migrations.
