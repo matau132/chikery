86ad3f425778f8f16226ce36c730d05239a8886a
Nam Nguyen <matau135@gmail.com>
2020-12-21 18:31:00 +0700

    whishlist

---
+++ database/migrations/2020_12_21_112348_whishlist.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Whishlist extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('whishlists', function (Blueprint $table) {
+            $table->bigInteger('customer_id')->unsigned();
+            $table->bigInteger('product_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->primary(['customer_id', 'product_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('whishlists');
+    }
+}
